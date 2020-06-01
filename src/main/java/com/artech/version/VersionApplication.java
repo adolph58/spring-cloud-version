5195732c0112d390d896ddf89e6c120443f19d26
程江涛 <jiangtao.cheng@orderplus.com>
2020-06-01 19:15:31 +0800

    init

---
+++ src/main/java/com/artech/version/VersionApplication.java
@@ -0,0 +1,15 @@
+package com.artech.version;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Arte
+ * @create 2020/6/1.
+ */
+@SpringBootApplication
+public class VersionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VersionApplication.class, args);
+    }
+}
